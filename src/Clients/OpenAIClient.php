bc20df69a00457b66698e0dcfe33dcab4d035441
Khien <kcp@cct-technology.com>
2025-03-07 08:55:28 +0700

    .

--- src/Clients/OpenAIClient.php
+++ src/Clients/OpenAIClient.php
@@ -4,12 +4,29 @@ declare(strict_types=1);
 
 namespace Mbvb1223\ChatAiMulticlient\Clients;
 
-use GuzzleHttp\Client;
+use OpenAI;
+use OpenAI\Client;
 
 class OpenAIClient extends BaseClient
 {
+    protected string $model = 'gpt-3.5-turbo';
+    private Client $client;
+
+    public function setClient(string $apiKey): void
+    {
+        $this->client = OpenAI::client($apiKey);
+    }
+
     public function chat(string $prompt, array $options = []): string
     {
-        return 'Openai is chatting';
+        $response = $this->client->chat()->create([
+            'model' => $this->getModel(),
+            'messages' => [
+                ['role' => 'system', 'content' => 'You are a helpful assistant.'],
+                ['role' => 'user', 'content' => 'Hello, ChatGPT!'],
+            ],
+        ]);
+
+        return $response->choices[0]->message->content;
     }
 }
